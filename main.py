62f8d33671818d9a4debf89d2b4f8fc2d68be82d
Hallows <yinxiangbo666@gmail.com>
2022-02-18 16:57:19 +0800

    GUI included

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 import logging
 from Module import GoonsJabber
 from Module import GoonsWorm
+import guizero as gui
 
 proxy = '127.0.0.1:7890'
 proxies = {
@@ -13,16 +14,20 @@ clientJid = 'azika_gaptain@goonfleet.com'
 toJid = 'directorbot@goonfleet.com'
 jabberPass = 'LOVE@alan1995'
 
-# if __name__ == '__main__':
-# Ideally use optparse or argparse to get JID,
-# password, and log level.
+if __name__ == '__main__':
+    # Ideally use optparse or argparse to get JID,
+    # password, and log level.
 
-# logging.basicConfig(level=logging.DEBUG,
-#                     format='%(levelname)-8s %(message)s')
+    # logging.basicConfig(level=logging.DEBUG,
+    #                     format='%(levelname)-8s %(message)s')
 
-# xmpp = GoonsJabber.EchoBot()
-# xmpp.connect()
-# xmpp.process(block=False)
-# time.sleep(10)
-# print(xmpp.boundjid)
-# xmpp.xmpp_send()
+    # xmpp = GoonsJabber.EchoBot()
+    # xmpp.connect()
+    # xmpp.process(block=False)
+    # time.sleep(10)
+    # print(xmpp.boundjid)
+    # xmpp.xmpp_send()
+
+    app = gui.App(title="BeeToolBox", layout='grid', width=750, height=500)
+    title_box = gui.TitleBox(app, "title",)
+    app.display()
